d5917b9a35018363301046dbabe73b20764d4b9c
VicenBayugar <vicentebayugar@gmail.com>
2022-05-24 12:34:56 -0300

    Agrego home container

---
+++ app/src/components/HomeContainer/HomeContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomeContainer.css";
+import ImageSlider from "../ImageSlider/ImageSlider";
+
+const HomeContainer = () => {
+  return (
+    <div className="home">
+      <ImageSlider />
+    </div>
+  );
+};
+
+export default HomeContainer;
