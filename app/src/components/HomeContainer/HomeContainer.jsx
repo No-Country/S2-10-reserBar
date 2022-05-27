e04e3543275bbc120331324da9f4a33ba17c2b5e
Andres Nicolas Rubio <rubioandres1@gmail.com>
2022-05-27 10:16:32 -0300

    Cards

--- app/src/components/HomeContainer/HomeContainer.jsx
+++ app/src/components/HomeContainer/HomeContainer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./HomeContainer.css";
 import BannerSlider from "../BannerSlider/BannerSlider"
+import { CardShowContainer } from "../CardShowContainer/CardShowContainer";
 
 const HomeContainer = () => {
   return (
     <div className="home">
-      
+    
       <BannerSlider/>
-      
     </div>
   );
 };
