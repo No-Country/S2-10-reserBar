2a8087ff8f97d1da51f0ef4d0aa2cbda776f9905
Andres Nicolas Rubio <rubioandres1@gmail.com>
2022-05-27 10:16:32 -0300

    Cards

--- app/src/services/bares.js
+++ app/src/services/bares.js
@@ -1,12 +1,22 @@
 const API_URL = "http://localhost:3005/api/bares";
 
 export async function getAllBares() {
-    try {
-      const response = await fetch(`${API_URL}`);
-      const data = await response.json();
-      return data.bares;
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const response = await fetch(`${API_URL}`);
+    const data = await response.json();
+    return data.bares;
+  } catch (error) {
+    console.error(error);
   }
-  
+}
+
+export async function getXBares(quantity) {
+  const bares = await getAllBares();
+  let baresByQuantity = [];
+
+  for (let index = 0; index < quantity; index++) {
+    baresByQuantity = [...baresByQuantity, bares[index]];
+  }
+
+  return baresByQuantity;
+}
