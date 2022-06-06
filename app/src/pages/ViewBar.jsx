94bf6273ead9103c55ed258b548d06b6e6dbccc1
LordArshen <m.r.barrera@hotmail.com>
2022-06-06 10:08:52 -0500

    page bar routes fixed

--- app/src/pages/ViewBar.jsx
+++ app/src/pages/ViewBar.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { useLocation } from "react-router-dom";
 import ImageSlider from "../components/ImageSlider/ImageSlider"
 import BarNav from "../components/BarNav/BarNav"
+import { useParams } from 
+"react-router-dom";
 
 const ViewBar = () => {
-  let infoBar = useLocation(); 
-  console.log(infoBar)
+   const [infoBar,setInfoBar] = useState({}) ; 
+  /* console.log(infoBar);  */
+  const {id} = useParams();
+  console.log(id); 
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  useEffect (() => {
+    fetch(`http://localhost:3005/api/bares/${id}`, requestOptions)
+      .then(response => response.json() )
+      .then(response => setInfoBar(response.bares))
+      .catch(error => console.log('error', error)); 
+      
+  }, [])
+  console.log(infoBar);
+  /*  */
+  
+  
+  
 
   return (
       <>
+        
+        <ImageSlider ImageProps={infoBar.photos} namePro={infoBar.name}  />
     
-        <ImageSlider ImageProps={infoBar.state.bar.photos} namePro={infoBar.state.bar.name}  />
-    
-        <BarNav description={[infoBar.state.bar]}
+        <BarNav description={infoBar}
 
         />
         
