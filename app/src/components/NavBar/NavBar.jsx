946e65527ad020a754bf7585823d7020169ff071
VicenBayugar <vicentebayugar@gmail.com>
2022-06-10 10:39:11 -0300

    Redirección a home y cambio en navbar

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -2,41 +2,43 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import logo from "../../assets/logo.svg";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
-    return (
-        <>
-            <div className="navbar">
-                <ul className="navLinks">
-                    <li>
-                        <NavLink to="/" className="navLink negrita">
-                            INICIO
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about" className="navLink negrita">
-                            QUIENES SOMOS
-                        </NavLink>
-                    </li>
+  const tokenUsuario = useSelector((state) => state.token.token);
+  return (
+    <>
+      <div className="navbar">
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/" className="navLink negrita">
+              INICIO
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className="navLink negrita">
+              QUIENES SOMOS
+            </NavLink>
+          </li>
 
-                    <NavLink to="/">
-                        <img src={logo} alt="logo" width={75} height={75} />
-                    </NavLink>
-                    <li>
-                        <NavLink to="/reservar" className="navLink negrita">
-                            RESERVAR
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/login" className="navLink negrita">
-                            INICIAR SESIÓN
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-            <hr></hr>
-        </>
-    );
+          <NavLink to="/">
+            <img src={logo} alt="logo" width={75} height={75} />
+          </NavLink>
+          <li>
+            <NavLink to="/reservar" className="navLink negrita">
+              RESERVAR
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" className="navLink negrita">
+              {tokenUsuario ? "MI PERFIL" : "INICIAR SESIÓN"}
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <hr></hr>
+    </>
+  );
 };
 
 export default NavBar;
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { CardShowContainer } from "../components/CardShowContainer/CardShowContainer";
 import HomeContainer from "../components/HomeContainer/HomeContainer";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getBares } from "../store/actions/baresActions";
 
 const Home = () => {
-  const tokenUsuario = useSelector((state) => state.token.token);
-  console.log(tokenUsuario);
-
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBares());
