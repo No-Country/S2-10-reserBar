c0c6c986475b5121057f28dd37d5ec5da8d8ced1
LordArshen <m.r.barrera@hotmail.com>
2022-06-15 10:18:49 -0500

    deploy settings githubpages

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'No-Country/S2-10-reserBar/tree/main/app'
 })
