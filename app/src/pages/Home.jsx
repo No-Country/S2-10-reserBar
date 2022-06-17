4986bcb124bb41900f0c3860ad28c14b9e9d81a3
leandrogutierrezdev <leandrogutierrezdev@gmail.com>
2022-06-17 16:53:06 -0300

    Por ahora me ganó

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { CardShowContainer } from "../components/CardShowContainer/CardShowContainer";
-import { HomeContainer } from "../components/HomeContainer/HomeContainer";
+import  HomeContainer from "../components/HomeContainer/HomeContainer";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getBares } from "../store/actions/baresActions";
