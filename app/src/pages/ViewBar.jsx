bf3a96d12b571b44fd5f0a2d784e3ace39e2de3d
leandrogutierrezdev <leandrogutierrezdev@gmail.com>
2022-06-24 14:42:58 -0300

    Slider terminado en bares

--- app/src/pages/ViewBar.jsx
+++ app/src/pages/ViewBar.jsx
@@ -1,39 +1,39 @@
-import React,{useState,useEffect} from "react";
-import { useLocation } from "react-router-dom";
-import ImageSlider from "../components/ImageSlider/ImageSlider"
-import BarNav from "../components/BarNav/BarNav"
-import { useParams } from 
-"react-router-dom";
-import { ReserveForm } from "../components/ReserveForm/ReserveForm";
+import React, { useState, useEffect } from "react";
+
+import BarNav from "../components/BarNav/BarNav";
+import { useParams } from "react-router-dom";
+
+import SlideLele from "../components/BannerSlider/SlideLele";
 
 const ViewBar = () => {
-   const [infoBar,setInfoBar] = useState({}) ; 
-  
-  const {id} = useParams();
-  
-  var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  useEffect (() => {
-    fetch(`https://reserbar-api.herokuapp.com/api/bares/${id}`, requestOptions)
-      .then(response => response.json() )
-      .then(response => setInfoBar(response.bares))
-      .catch(error => console.log('error', error)); 
-      
-  }, [])
-   
+    const [infoBar, setInfoBar] = useState({});
+    const [photos, setPhotos] = useState([]);
 
-  return (
-      <>
-        
-        <ImageSlider ImageProps={infoBar.photos} namePro={infoBar.name}  />
-    
-        <BarNav description={infoBar}/>
+    const { id } = useParams();
 
-        
-    </> 
-  )
+    var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
+    useEffect(() => {
+        fetch(
+            `https://reserbar-api.herokuapp.com/api/bares/${id}`,
+            requestOptions
+        )
+            .then((response) => response.json())
+            .then((response) => {
+                setInfoBar(response.bares), setPhotos(response.bares.photos);
+            })
+            .catch((error) => console.log("error", error));
+    }, []);
+
+    return (
+        <>
+            <SlideLele images={photos} nombreBar={infoBar.name} />
+
+            <BarNav description={infoBar} />
+        </>
+    );
 };
 
 export default ViewBar;
